4949d0ec16272776f4a5e27f84c5d10688611f0d
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-01-09 13:34:20 -0600

    Visualizacion en la pagina del contenido

--- src/app/proyectos/p2/p2.component.ts
+++ src/app/proyectos/p2/p2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-p2',
@@ -23,24 +23,59 @@ import { Component } from '@angular/core';
 })
 
 export class P2Component {
+  
+  @Output() texto = new EventEmitter<string>();
+  contenido = ''
 
+  AbrirArchivo(doc : Event){
+    const loc =  doc.target
+    console.log( loc );
+    this.leerArchivo( loc )
 
-  AbrirArchivo(event: Event){
-    const archivo = event.target;
-    this.leerArchivo(archivo);
   }
-  leerArchivo(doctxt: any){
-    const file = doctxt.files[0];
-    const lector = new FileReader();
-    lector.onload = function(e){
-      var texto = lector.result;
-      console.log(texto);
-      
-      return texto;
+  leerArchivo( value : any ){
+    const file : File = value.files[0];
+    console.log( file )
+    const leer = new FileReader();
+    leer.onload = () => {
+      const contenido = leer.result
+      console.log( contenido  )
+      this.mostrar( contenido )
     }
-    lector.readAsText(file)
-
+    leer.readAsText( file )
+    
   }
+  mostrar( txt: any){
+    this.contenido =  txt;
+  }
+
+
+
+
+
+
+
+  // AbrirArchivo(event: Event){
+  //   const archivo = event.target;
+  //   this.leerArchivo(archivo);
+  // }
+  // leerArchivo(doctxt: any){
+  //   const file = doctxt.files[0];
+  //   const lector = new FileReader();
+  //   lector.onload = function(e){
+  //     var texto = lector.result;
+  //     console.log(texto);
+      
+  //     return texto;
+  //   };
+  //   lector.readAsText(file)
+  //   this.txt(lector);
+
+  // }
+  // txt( lect: any){
+  //   lect
+  //   console.log(" Esto es una prueba*********************************************")
+  // }
   
   // leerArchivo($event: any){
   //   this.readThis($event.target)

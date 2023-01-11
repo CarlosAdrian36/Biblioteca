a61db203c7c7a6231b0f2d77183224d5ad75518a
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-01-11 09:26:31 -0600

    Creacion de exmen.interface

---
+++ src/app/proyectos/interface/examen.interface.ts
@@ -0,0 +1,13 @@
+export interface examen{
+    pregunta: string,
+    respuestas: Respuestas,
+    respuestaCorrecta: string,
+  }
+
+export interface Respuestas {
+    r1: string;
+    r2: string;
+    r3: string;
+    r4: string;
+}
+

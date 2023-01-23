f875dab30e1b8251486e236cfc0859244a6c1f68
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-01-23 17:25:30 -0600

    Creacion de la seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PageErrorComponent } from './shared/page-error/page-error.component';
-import { P3Component } from './app/proyectos/p3/p3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageErrorComponent,
-    P3Component
+
     
   ],
   imports: [
--- src/app/proyectos/proyectos-routing.module.ts
+++ src/app/proyectos/proyectos-routing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { P1Component } from './p1/p1.component';
 import { P2Component } from './p2/p2.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { P3Component } from './p3/p3.component';
 
 const routes : Routes =[
 
@@ -18,6 +19,10 @@ const routes : Routes =[
       {
         path:'Proyecto2',
         component: P2Component
+      },
+      {
+        path: 'Proyecto3',
+        component: P3Component
       },
       {
         path: '**',

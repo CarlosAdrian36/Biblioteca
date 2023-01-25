239dbd70b63f7813e45540b75b2c8ea15f93d0ab
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-01-25 11:33:44 -0600

    Creacion de la clase oracion

--- src/app/proyectos/interface/examen.interface.ts
+++ src/app/proyectos/interface/examen.interface.ts
@@ -10,39 +10,69 @@ export class parrafo {
     this._correcta = correcta
     this._respuestas = respuestas
   }
-  
-  
   public get respuestas() : string[] {
     return this._respuestas;
   }
-  
-
-
   public set respuestas(v: string[]) {
     this._respuestas = v;
   }
-
-
-
-
-
   public get correcta(): number {
     return this._correcta
   }
-
-
   public set correcta(v: number) {
     this._correcta = v;
   }
-
-
-
   public get verpregunta(): string {
     return this._pregunta;
   }
   public set pregunta(v: string) {
     this._pregunta = v;
   }
+}
+export class oracion{
+
+  private _oracion: string[];
+  private _respuestas: string[];
+  private _correcta: string;
+
+  constructor(oracion :  string [], respuesta: string[], correcta:string ){
+    this._oracion = oracion;
+    this._respuestas = respuesta;
+    this._correcta = correcta;
+  }
+
+  
+  public get getOracion() : string[] {
+    return this._oracion;
+  }
+  
+  public set setOracion(v : string[]) {
+    this._oracion = v;
+  }
+  
+  public get getRespuestas() : string[] {
+    return this._respuestas
+  }
+  
+  public set setRespuestas(v : string[]) {
+    this._respuestas = v;
+  }
+  
+  public get getCorrecta () : string {
+    return this._correcta
+  }
+  
+  public set setCorrecta(v : string) {
+    this._correcta = v;
+  }
+  
+  
+  
+  
+  
+  
+
+
 
 }
 
--- src/app/proyectos/p4/p4.component.ts
+++ src/app/proyectos/p4/p4.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./p4.component.css']
 })
 export class P4Component {
+  contenido: string = ''
+
+  AbrirArchivo(doc: Event) {
+    const loc = doc.target
+    this.leerArchivo(loc)
+  }
+  leerArchivo(value: any) {
+    const file: File = value.files[0];
+    const leer = new FileReader();
+    leer.onload = () => {
+      const contenido = leer.result;
+      this.mostrar(contenido);
+      this.separar(contenido);
+    }
+    leer.readAsText(file);
+  }
+
+  mostrar(txt: any) {
+    this.contenido = txt;
+  }
+
+  separar(txt: any) {
+    const mensaje: string = txt;
+    const lineas = mensaje.split(/\r\n|\r | _ /);
+    console.log(lineas)
+    this.construir(lineas)
+  }
+
+  construir(linea: string[]){
+
+    
+
+
+  }
+
+
+
 
 }

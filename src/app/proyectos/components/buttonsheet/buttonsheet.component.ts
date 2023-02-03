da0775ff502aaebb85f61ea644d1eb122102f20f
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-02-03 15:40:35 -0600

    Creacion de component button y actualizacion del botton para subir archivo

--- src/app/proyectos/components/buttonsheet/buttonsheet.component.ts
+++ src/app/proyectos/components/buttonsheet/buttonsheet.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatBottomSheetRef, MatBottomSheet } from '@angular/material/bottom-sheet';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-buttonsheet',
   templateUrl: './buttonsheet.component.html',
-  styles: [
-  ]
+  styleUrls: ['./buttonsheet.component.css']
 })
 export class ButtonsheetComponent {
-
+  @ViewChild('fileInput') xx : any;
   constructor(private _bottomSheetRef: MatBottomSheetRef<MatBottomSheet>) {}
+doc : any;
+
+
 
   openLink(event: MouseEvent): void {
     this._bottomSheetRef.dismiss();
     event.preventDefault();
+    console.log(event)
   }
+
+
+  AbrirArchivo(doc: Event ){
+    
+    console.log(doc)
+
+  }
+  
 
 }

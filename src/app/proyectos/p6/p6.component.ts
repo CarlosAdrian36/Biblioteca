960553ec5b36a0fb92ef4e9c3483f37aad07d346
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2023-02-23 15:16:52 -0600

    Visaulizacion de preguntas limpia

--- src/app/proyectos/p6/p6.component.ts
+++ src/app/proyectos/p6/p6.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { ButtonsheetComponent } from '../components/buttonsheet/buttonsheet.component';
 
 @Component({
   selector: 'app-p6',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./p6.component.css']
 })
 export class P6Component {
+
+  contenido: string = ''
+  correcta = ''
+
+  AbrirArchivo(doc: Event) {
+    const loc = doc.target
+    this.leerArchivo(loc)
+  }
+  constructor(private bottomSheet : MatBottomSheet){}
+
+  open(){
+    this.bottomSheet.open(ButtonsheetComponent).afterDismissed().subscribe(
+    result => {
+      if(result) {
+
+        this.leerArchivo(result);
+      }
+    });
+
+  }
+  leerArchivo(value: any) {
+    const leer = new FileReader();
+    leer.onload = () => {
+      // this.Oraciones = []
+      const contenido = leer.result;
+      this.mostrar(contenido);
+      this.separar(contenido);
+    }
+    leer.readAsText(value);
+  }
+
+  mostrar(txt: any) {
+    this.contenido = txt;
+  }
+
+  separar(txt: any) {
+    const mensaje: string = txt;
+    // const lineas = mensaje.split(/.\r\n/gi);
+    console.log(mensaje)
+    this.construir(mensaje)
+  }
+
+  construir(mensaje : string){
+    const preguntas = mensaje.split(/\s+{.+}\s+/);
+    console.log(preguntas);
+    // const respuestas = s
+  }
+
 
 }
